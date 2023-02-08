f2240571f9180f96848e7b77989c9f551cd0f827
CicchelleroAlessandro-3BI-2020-2021 <73578558+CicchelleroAlessandro-3BI-2020-2021@users.noreply.github.com>
2023-02-08 12:46:01 +0100

    Add files via upload

---
+++ MedicoPaziente/src/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author 18587
+ */
+public class Main {
+    public static void main(String[] args) {
+        Medico med1 = new Medico("Bob", "Marley", "MRLBBO80A01L781G", "Ononcologia");
+        Medico med2 = new Medico("Larry", "Dabney", "DBNLRY80A01L103F");
+
+        Paziente paz1 = new Paziente("1Axe", "Igne", "37125", "XAEGNI80A01F205H");
+        Paziente paz2 = new Paziente("2Axe", "Igne", "37125", "XAEGNI80A01F205H");
+        Paziente paz3 = new Paziente("3Axe", "Igne", "37125", "XAEGNI80A01F205H");
+        Paziente paz4 = new Paziente("4Axe", "Igne", "37125", "XAEGNI80A01F205H");
+        Paziente paz5 = new Paziente("4Axe", "Igne", "37125", "XAEGNI80A01F205H");
+        paz1.setMedicoFamigliaById(2L);
+        paz4.setMedicoFamigliaById(2L);
+        paz3.setMedicoFamigliaById(2L);
+        paz2.setMedicoFamigliaById(1L);
+        paz5.setMedicoFamigliaById(1L);
+        System.out.println(paz1.toString());
+        System.out.println(paz1.toStringPazienteMedico());
+        System.out.println(med2.toStringMedicoPazienti());
+    }
+
+}
